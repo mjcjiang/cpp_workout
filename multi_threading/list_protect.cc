3a426c72cd99dec8853c7c8de6f590235a31fafb
mjcjiang <redmagic039@gmail.com>
2023-08-21 23:38:36 +0800

    commit

--- multi_threading/list_protect.cc
+++ multi_threading/list_protect.cc
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include <iostream>
 #include <random>
+#include <thread>
 
 using namespace std::chrono;
 
@@ -34,7 +35,13 @@ public:
         stamp_ = get_system_timestamp();
         value_ = gen_random(1, 100);
     }
-private:
+
+    void show_record() {
+        std::cout << "stamp: " << stamp_ << std::endl;
+        std::cout << "value: " << value_ << std::endl;
+        std::cout << "thread_name: " << thread_name_ << std::endl;
+    }
+  private:
     std::string stamp_;
     int value_;
     std::string thread_name_;
@@ -46,12 +53,42 @@ public:
         std::lock_guard<std::mutex> guard(list_mutex_);
         record_list_.push_back(LogRecord(thread_name));
     }
-private:
+
+    void show_list() {
+        std::cout << "--------------------------------" << std::endl;
+        for (auto &e : record_list_) {
+            e.show_record();
+        }
+        std::cout << "--------------------------------" << std::endl;
+    }
+  private:
     std::list<LogRecord> record_list_;
     std::mutex list_mutex_;
 };
 
+void add_to_list(RecordList& lst, const std::string &thread_name) {
+    for (int i = 0; i < 10; ++i) {
+        lst.add_to_list(thread_name);
+        std::this_thread::sleep_for(seconds(1));
+    }
+}
+
+void tell_list(RecordList &lst) {
+    for (int i = 0; i < 5; ++i) {
+        lst.show_list();
+        std::this_thread::sleep_for(seconds(3));
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    RecordList records;
+    std::thread write_thread1(add_to_list, std::ref(records), "write_thread1");
+    std::thread write_thread2(add_to_list, std::ref(records), "write_thread2");
+    std::thread tell_thread(tell_list, std::ref(records));
+
+    write_thread1.join();
+    write_thread2.join();
+    tell_thread.join();
     return 0;
 }
